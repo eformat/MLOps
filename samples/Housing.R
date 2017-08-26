df3fa7bb78821a267f7e0da9609f70c174585569
Stefano Picozzi <StefanoPicozzi@gmail.com>
2017-08-27 09:50:15 +1000

    Create Housing.R

---
+++ samples/Housing.R
@@ -0,0 +1,151 @@
+
+
+sqlContext <- sparkRSQL.init(sc)
+
+housing_a_file_path <- file.path('', 'nfs','data','2013-acs','ss13husa.csv')
+housing_b_file_path <- file.path('', 'nfs','data','2013-acs','ss13husb.csv')
+
+housing_a_df <- read.df(sqlContext, 
+                            housing_a_file_path, 
+                            header='true', 
+                            source = "com.databricks.spark.csv", 
+                            inferSchema='true')
+
+
+housing_b_df <- read.df(sqlContext, 
+                            housing_b_file_path, 
+                            header='true', 
+                            source = "com.databricks.spark.csv", 
+                            inferSchema='true')
+
+
+housing_df <- rbind(housing_a_df, housing_b_df)
+nrow(housing_df)
+head(housing_df)
+
+library(ggplot2)
+
+housing_region_df_local <- collect(select(housing_df,"REGION"))
+str(housing_region_df_local)
+
+housing_region_df_local$REGION <- factor(
+        x=housing_region_df_local$REGION, 
+        levels=c(1,2,3,4,9),
+        labels=c('Northeast', 'Midwest','South','West','Puerto Rico')
+)
+
+c <- ggplot(data=housing_region_df_local, aes(x=factor(REGION)))
+
+c + geom_bar() + xlab("Region")
+
+collect(select(housing_df, "REGION", "VALP"))
+
+system.time(
+        collect(select(housing_df, housing_df$VALP))
+)
+
+system.time(
+        collect(select(housing_df, "VALP"))
+)
+
+head(selectExpr(housing_df, "(VALP / 100) as VALP_by_100"))
+
+system.time(
+        housing_valp_1000 <- collect(filter(select(housing_df, "REGION", "VALP"), "VALP > 1000"))
+)
+
+housing_valp_1000
+
+registerTempTable(housing_df, "housing")
+
+system.time(
+        housing_valp_1000_sql <- collect(sql(sqlContext, "SELECT REGION, VALP FROM housing WHERE VALP >= 1000"))
+)
+
+housing_valp_1000_sql
+
+system.time(
+        housing_valp_1000_subset <- collect(subset(
+            housing_df, housing_df$VALP>1000, 
+            c("REGION","VALP")
+        ))
+)
+
+housing_valp_1000_subset
+
+system.time(
+        housing_valp_1000_bracket <- collect(
+            housing_df[housing_df$VALP>1000, c("REGION","VALP")]
+        )
+)
+
+housing_valp_1000_bracket
+housing_valp_1000_bracket$REGION <- factor(
+        x=housing_valp_1000_bracket$REGION, 
+        levels=c(1,2,3,4,9),
+        labels=c('Northeast', 'Midwest','South','West','Puerto Rico')
+)
+c <- ggplot(data=housing_region_df_local, aes(x=factor(REGION)))
+c + geom_bar() + ggtitle("Samples with VALP>1000") + xlab("Region")
+
+collect(select(housing_df, avg(housing_df$VALP)))
+
+housing_avg_valp <- collect(agg(
+        groupBy(housing_df, "REGION"), 
+        NUM_PROPERTIES=n(housing_df$REGION),
+        AVG_VALP = avg(housing_df$VALP), 
+        MAX_VALUE=max(housing_df$VALP),
+        MIN_VALUE=min(housing_df$VALP)
+))
+housing_avg_valp$REGION <- factor(
+        housing_avg_valp$REGION, 
+        levels=c(1,2,3,4,9), 
+        labels=c('Northeast', 'Midwest','South','West','Puerto Rico')
+)
+housing_avg_valp
+
+housing_avg_valp <- collect(agg(
+        groupBy(housing_df, "REGION", "BDSP"), 
+        NUM_PROPERTIES=n(housing_df$REGION),
+        AVG_VALP = avg(housing_df$VALP), 
+        MAX_VALUE=max(housing_df$VALP),
+        MIN_VALUE=min(housing_df$VALP)
+))
+housing_avg_valp$REGION <- factor(
+        housing_avg_valp$REGION, 
+        levels=c(1,2,3,4,9), 
+        labels=c('Northeast', 'Midwest','South','West','Puerto Rico')
+)
+housing_avg_valp
+
+head(arrange(select(housing_df, "REGION", "VALP"), desc(housing_df$VALP)))
+housing_avg_agg <- agg(
+            groupBy(housing_df, "REGION", "BDSP"), 
+            NUM_PROPERTIES=n(housing_df$REGION),
+            AVG_VALP = avg(housing_df$VALP), 
+            MAX_VALUE=max(housing_df$VALP),
+            MIN_VALUE=min(housing_df$VALP)
+)
+housing_avg_sorted <- head(arrange(
+           housing_avg_agg,   
+           desc(housing_avg_agg$AVG_VALP)
+))
+    
+housing_avg_sorted$REGION <- factor(
+        housing_avg_sorted$REGION, 
+        levels=c(1,2,3,4,9), 
+        labels=c('Northeast', 'Midwest','South','West','Puerto Rico')
+)
+housing_avg_sorted
+
+
+
+
+
+
+
+
+
+
+
+
