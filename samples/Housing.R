8ba21350ddddc139391938606fdd8061e67b0bb0
Stefano Picozzi <StefanoPicozzi@gmail.com>
2017-08-29 07:51:42 +1000

    Update Housing.R

--- samples/Housing.R
+++ samples/Housing.R
@@ -1,20 +1,19 @@
 Sys.setenv(SPARK_HOME ="/home/guest/spark-2.1.0")
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+
 sc <- sparkR.session(master = "spark://172.30.253.81:7077", 
-                     sparkConfig = list(spark.driver.memory = "1g"), 
-                     sparkPackages="com.databricks:spark-csv_2.11:1.2.0")
+               sparkConfig = list(spark.driver.memory="1g"),
+               sparkJars = c("org.apache.hadoop:hadoop-aws:2.7.3"),
+               sparkPackages = c("com.databricks:spark-csv_2.11:1.2.0")
+                    )
 
-housing_a_file_path <- file.path('/home/guest', 'housing', 'ss13husa.csv')
-housing_b_file_path <- file.path('/home/guest', 'housing', 'ss13husb.csv')
+#housing_a_file_path <- file.path('/home/guest', 'housing', 'ss13husa.csv')
+#housing_b_file_path <- file.path('/home/guest', 'housing', 'ss13husb.csv')
+housing_a_file_path <- file.path('s3://bit_bucket', 'housing', 'ss13husa.csv')
+housing_b_file_path <- file.path('s3://bit_bucket', 'housing', 'ss13husb.csv')
 
-a_df <- read.csv(housing_a_file_path)
-b_df <- read.csv(housing_b_file_path)
-
-housing_a_df <- createDataFrame(a_df)
-housing_b_df <- createDataFrame(b_df)
-
-#housing_a_df <- read.df(housing_a_file_path, source = "csv", header="true", inferSchema = "true")
-#housing_b_df <- read.df(housing_b_file_path, source = "csv", header="true", inferSchema = "true")
+housing_a_df <- read.df(housing_a_file_path, source = "csv", header="true", inferSchema = "true")
+housing_b_df <- read.df(housing_b_file_path, source = "csv", header="true", inferSchema = "true")
 
 housing_df <- rbind(housing_a_df, housing_b_df)
 nrow(housing_df)
