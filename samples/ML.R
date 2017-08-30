6b7ab80d0917c8fe6f6e95a473d60e6eac1c4c13
Stefano Picozzi <StefanoPicozzi@gmail.com>
2017-08-30 22:15:27 +1000

    Update ML.R

--- samples/ML.R
+++ samples/ML.R
@@ -1,4 +1,4 @@
-Sys.setenv(SPARK_HOME ="/home/guest/spark-2.1.0")
+Sys.setenv(SPARK_HOME ="/opt/spark")
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
 sparkR.session(master = "spark://172.17.0.6:7077", sparkConfig = list(spark.driver.memory = "1g"))
 library("magrittr")
