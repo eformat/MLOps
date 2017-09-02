a11eef092ded333ce8b865f2d0503300fc8733ce
Stefano Picozzi <StefanoPicozzi@gmail.com>
2017-09-02 17:11:22 +1000

    Update WendyYu.R

--- samples/WendyYu.R
+++ samples/WendyYu.R
@@ -5,7 +5,9 @@ Sys.setenv(SPARK_HOME ="/opt/spark")
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
 
 # Edit to reflect your Spark master end point
-sparkR.session(master = "spark://172.17.0.6:7077", sparkConfig = list(spark.driver.memory = "1g"))
+sparkR.session(master = "spark://172.17.0.6:7077", 
+               appName = "Wendy",
+               sparkConfig = list(spark.driver.memory = "1g"))
 library("magrittr")
 
 # 1.
